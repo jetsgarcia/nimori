e63c5277c1e2cce4c9a855e552b1dae308944926
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-07 17:10:59 +0800

    Add route for search page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { Route, Routes } from "react-router";
+import "@/App.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import NavBar from "@/components/navbar";
+import SearchPage from "@/pages/SearchPage";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <>
+        <NavBar />
+        <Routes>
+          <Route path="search" element={<SearchPage />} />
+        </Routes>
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
