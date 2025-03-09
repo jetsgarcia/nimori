556195980c5a98646742832bb2dec8b71c88c27f
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-09 12:12:25 +0800

    Add Apollo Client for GraphQL integration for Anilist API

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://graphql.anilist.co",
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
