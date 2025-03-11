0b3d3aa3447d634d86938cd6b2d697f7e73cb787
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-11 13:20:13 +0800

    Remove ThemeProvider from App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,17 @@
 import { Route, Routes } from "react-router";
-import { ThemeProvider } from "@/components/theme-provider";
 import NavBar from "@/components/navbar";
 import SearchPage from "@/pages/search/SearchPage";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <>
       <NavBar />
       <div className="px-4 pt-13 md:px-8 md:pt-18">
         <Routes>
           <Route path="search" element={<SearchPage />} />
         </Routes>
       </div>
-    </ThemeProvider>
+    </>
   );
 }
 
