8a8ceda53cc27d355d7429f94c42f1310f01192a
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-11 09:56:13 +0800

    Adjust padding

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,17 @@
 import { Route, Routes } from "react-router";
-import "@/App.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import NavBar from "@/components/navbar";
-import SearchPage from "@/pages/SearchPage";
+import SearchPage from "@/pages/search/SearchPage";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <>
-        <NavBar />
+      <NavBar />
+      <div className="px-4 pt-13 md:px-8 md:pt-18">
         <Routes>
           <Route path="search" element={<SearchPage />} />
         </Routes>
-      </>
+      </div>
     </ThemeProvider>
   );
 }
