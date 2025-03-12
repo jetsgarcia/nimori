e7789ff5e7113abbf22549714660f2259d8f0da4
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-12 14:35:36 +0800

    Fix bug where page is being shown before navigating to homepage when logged in

--- client/src/pages/landing/LandingPage.tsx
+++ client/src/pages/landing/LandingPage.tsx
@@ -1,21 +1,26 @@
 import { useAuth } from "@clerk/clerk-react";
 import { useNavigate } from "react-router";
-import { useEffect } from "react";
 import LandingTextSection from "./_components/landing-text-section";
 import LandingImageSection from "./_components/landing-image-section";
+import Loading from "@/components/loading";
 
 export default function LandingPage() {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
   const navigate = useNavigate();
 
-  useEffect(() => {
+  if (isLoaded) {
     if (isSignedIn) navigate("/home");
-  }, [isSignedIn, navigate]);
+    return (
+      <div className="bg-bg-light dark:bg-bg-dark flex h-screen justify-between overflow-hidden">
+        <LandingTextSection />
+        <LandingImageSection />
+      </div>
+    );
+  }
 
   return (
-    <div className="bg-bg-light dark:bg-bg-dark flex h-screen justify-between overflow-hidden">
-      <LandingTextSection />
-      <LandingImageSection />
+    <div className="grid h-screen w-full place-items-center">
+      <Loading />
     </div>
   );
 }
