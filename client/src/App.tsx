0068f5852e3926195f48568b375bd6c35744ceb7
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-13 13:14:25 +0800

    Add watchlist page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import NavBar from "@/components/navbar";
 import SearchPage from "@/pages/search/SearchPage";
 import { Toaster } from "@/components/ui/sonner";
+import WatchlistPage from "@/pages/watchlist/WatchlistPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="px-4 pt-13 md:px-8 md:pt-18">
         <Routes>
           <Route path="search" element={<SearchPage />} />
+          <Route path="watchlist" element={<WatchlistPage />} />
         </Routes>
       </div>
       <Toaster />
