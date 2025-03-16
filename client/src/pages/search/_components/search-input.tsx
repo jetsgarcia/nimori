3fd32e629edbc9a96f4f1c013e3f2eb6d6f98619
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-16 09:54:12 +0800

    Add error handling for search input if its empty

--- client/src/pages/search/_components/search-input.tsx
+++ client/src/pages/search/_components/search-input.tsx
@@ -1,6 +1,16 @@
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { TriangleAlert } from "lucide-react";
 
 interface SearchInputProps {
   triggerSearch: (search: string) => void;
@@ -8,6 +18,12 @@ interface SearchInputProps {
 
 export default function SearchInput({ triggerSearch }: SearchInputProps) {
   const [search, setSearch] = useState<string>("");
+  const [open, setOpen] = useState(false);
+
+  function handleSearch() {
+    if (search.length && search.trim().length) triggerSearch(search);
+    else setOpen(true);
+  }
 
   return (
     <div className="mb-6 flex flex-col gap-2 sm:flex-row">
@@ -17,14 +33,29 @@ export default function SearchInput({ triggerSearch }: SearchInputProps) {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            triggerSearch(search);
-          }
+          if (e.key === "Enter") handleSearch();
         }}
       />
-      <Button onClick={() => triggerSearch(search)} className="cursor-pointer">
+      <Button onClick={() => handleSearch()} className="cursor-pointer">
         Search
       </Button>
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle className="flex items-center gap-2 text-red-400">
+              <TriangleAlert size={20} /> <span>Error</span>
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              Please enter a valid search term.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction onClick={() => setOpen(false)}>
+              OK
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
