7894ec521152e91721ddd8adf23a7c7a6b16ca43
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-16 13:09:14 +0800

    Refactor confirmation dialog to use reusable component

--- client/src/components/anime-card.tsx
+++ client/src/components/anime-card.tsx
@@ -13,6 +13,7 @@ import {
 } from "./ui/select";
 import { cn } from "@/lib/utils";
 import useFetchWatchlist from "@/hooks/useFetchWatchlist";
+import DeleteConfirmationDialog from "./delete-confirmation-dialog";
 
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -34,40 +35,49 @@ export default function AnimeCard({
     useState<number>();
   const [watchStatus, setWatchStatus] = useState("toWatch");
   const [watchlistFromDB] = useFetchWatchlist();
+  const [isDeleteConfirmationVisible, setIsDeleteConfirmationDialogVisible] =
+    useState(false);
   const { user } = useUser();
   const { getToken } = useAuth();
 
+  async function removeAnimeFromWatchlist() {
+    const errorMessage = "Failed to remove anime to watchlist.";
+    try {
+      if (!user) return toast.error("User not found. Please sign in.");
+      const url = `${apiBaseUrl}/api/users/${user.id}/watchlist`;
+      const token = await getToken();
+
+      const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        mode: "cors",
+        body: JSON.stringify({ data: { animeId: anime.id } }),
+      });
+      if (!response.ok) return toast.error(errorMessage);
+      setAddedAnimeToWatchlist(undefined);
+      setRemovedAnimeToWatchlist(anime.id);
+      if (setAnimeList) {
+        setAnimeList((prev) => prev.filter((a) => a.id !== anime.id));
+      }
+    } catch {
+      toast.error(errorMessage);
+    }
+  }
+
   async function handleWatchlist() {
     if (!user) return toast.error("User not found. Please sign in.");
-    const url = `${apiBaseUrl}/api/users/${user.id}/watchlist`;
     const token = await getToken();
     if (
       watchlistFromDB.includes(anime.id) ||
       addedAnimeToWatchlist === anime.id
     ) {
-      // Removing anime from watchlist
-      const errorMessage = "Failed to remove anime to watchlist.";
-      try {
-        const response = await fetch(url, {
-          method: "DELETE",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-          mode: "cors",
-          body: JSON.stringify({ data: { animeId: anime.id } }),
-        });
-        if (!response.ok) return toast.error(errorMessage);
-        setAddedAnimeToWatchlist(undefined);
-        setRemovedAnimeToWatchlist(anime.id);
-        if (setAnimeList) {
-          setAnimeList((prev) => prev.filter((a) => a.id !== anime.id));
-        }
-      } catch {
-        toast.error(errorMessage);
-      }
+      setIsDeleteConfirmationDialogVisible(true);
     } else {
       // Adding anime to watchlist
+      const url = `${apiBaseUrl}/api/users/${user.id}/watchlist`;
       const errorMessage = "Failed to add anime to watchlist.";
 
       try {
@@ -91,84 +101,93 @@ export default function AnimeCard({
   }
 
   return (
-    <div className="hover:ring-primary-light dark:hover:ring-primary-dark flex h-full flex-col overflow-hidden rounded-lg bg-white shadow-lg transition duration-300 ease-in-out hover:shadow-xl hover:ring dark:bg-gray-900">
-      <img
-        src={anime.coverImage.extraLarge}
-        alt={anime.title.romaji}
-        height={385}
-        className="h-[385px] w-full object-cover"
+    <>
+      <DeleteConfirmationDialog
+        isDeleteConfirmationVisible={isDeleteConfirmationVisible}
+        setIsDeleteConfirmationDialogVisible={
+          setIsDeleteConfirmationDialogVisible
+        }
+        deleteAction={removeAnimeFromWatchlist}
       />
+      <div className="hover:ring-primary-light dark:hover:ring-primary-dark flex h-full flex-col overflow-hidden rounded-lg bg-white shadow-lg transition duration-300 ease-in-out hover:shadow-xl hover:ring dark:bg-gray-900">
+        <img
+          src={anime.coverImage.extraLarge}
+          alt={anime.title.romaji}
+          height={385}
+          className="h-[385px] w-full object-cover"
+        />
 
-      <div className="flex flex-grow flex-col justify-between p-4">
-        <h2 className="mb-2 line-clamp-2 text-lg">
-          {titleType === "english"
-            ? anime.title.english || anime.title.romaji
-            : anime.title.romaji}
-        </h2>
+        <div className="flex flex-grow flex-col justify-between p-4">
+          <h2 className="mb-2 line-clamp-2 text-lg">
+            {titleType === "english"
+              ? anime.title.english || anime.title.romaji
+              : anime.title.romaji}
+          </h2>
 
-        <div>
-          <div className="flex justify-between text-sm text-gray-600 dark:text-gray-400">
-            <span>
-              {anime.format === "MOVIE"
-                ? "Movie"
-                : anime.episodes
-                  ? `${anime.episodes} ${anime.episodes > 1 ? "Episodes" : "Episode"}`
-                  : "Ep count unknown"}
-            </span>
-            <span>{anime.startDate?.year ?? "No date"}</span>
-          </div>
+          <div>
+            <div className="flex justify-between text-sm text-gray-600 dark:text-gray-400">
+              <span>
+                {anime.format === "MOVIE"
+                  ? "Movie"
+                  : anime.episodes
+                    ? `${anime.episodes} ${anime.episodes > 1 ? "Episodes" : "Episode"}`
+                    : "Ep count unknown"}
+              </span>
+              <span>{anime.startDate?.year ?? "No date"}</span>
+            </div>
 
-          <div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            Status:{" "}
-            {anime.status
-              ? anime.status.charAt(0).toUpperCase() +
-                anime.status.slice(1).toLowerCase()
-              : "No status"}
-          </div>
+            <div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
+              Status:{" "}
+              {anime.status
+                ? anime.status.charAt(0).toUpperCase() +
+                  anime.status.slice(1).toLowerCase()
+                : "No status"}
+            </div>
 
-          <div className="flex gap-1">
-            {cardType === "watchlist" && (
-              <Select value={watchStatus} onValueChange={setWatchStatus}>
-                <SelectTrigger className="flex-grow cursor-pointer" size="sm">
-                  <SelectValue />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="toWatch">To watch</SelectItem>
-                  <SelectItem value="watching">Watching</SelectItem>
-                  <SelectItem value="watched">Watched</SelectItem>
-                </SelectContent>
-              </Select>
-            )}
-            <Button
-              size="sm"
-              className={cn(
-                "cursor-pointer",
-                cardType === "search" && "flex-grow",
+            <div className="flex gap-1">
+              {cardType === "watchlist" && (
+                <Select value={watchStatus} onValueChange={setWatchStatus}>
+                  <SelectTrigger className="flex-grow cursor-pointer" size="sm">
+                    <SelectValue />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="toWatch">To watch</SelectItem>
+                    <SelectItem value="watching">Watching</SelectItem>
+                    <SelectItem value="watched">Watched</SelectItem>
+                  </SelectContent>
+                </Select>
               )}
-              variant={cardType === "watchlist" ? "destructive" : "default"}
-              onClick={handleWatchlist}
-            >
-              {(watchlistFromDB.includes(anime.id) &&
-                removedAnimeToWatchlist !== anime.id) ||
-              addedAnimeToWatchlist === anime.id ? (
-                <>
-                  {cardType === "watchlist" && <Trash />}
-                  {cardType === "search" && (
-                    <>
-                      <Bookmark className="fill-bg-dark" />{" "}
-                      <span>Remove from watchlist</span>
-                    </>
-                  )}
-                </>
-              ) : (
-                <>
-                  <Bookmark /> <span>Add to watchlist</span>
-                </>
-              )}
-            </Button>
+              <Button
+                size="sm"
+                className={cn(
+                  "cursor-pointer",
+                  cardType === "search" && "flex-grow",
+                )}
+                variant={cardType === "watchlist" ? "destructive" : "default"}
+                onClick={handleWatchlist}
+              >
+                {(watchlistFromDB.includes(anime.id) &&
+                  removedAnimeToWatchlist !== anime.id) ||
+                addedAnimeToWatchlist === anime.id ? (
+                  <>
+                    {cardType === "watchlist" && <Trash />}
+                    {cardType === "search" && (
+                      <>
+                        <Bookmark className="fill-bg-light dark:fill-bg-dark" />{" "}
+                        <span>Remove from watchlist</span>
+                      </>
+                    )}
+                  </>
+                ) : (
+                  <>
+                    <Bookmark /> <span>Add to watchlist</span>
+                  </>
+                )}
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
